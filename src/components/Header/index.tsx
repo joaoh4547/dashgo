798773736809114c0467b02b93e8b7156f0b949f
João Henrique <joaohenriquetdj@gmail.com>
2021-12-11 15:38:12 -0300

    add responsivity in header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,9 +1,11 @@
-import { Flex } from "@chakra-ui/react";
+import { Flex, useBreakpointValue } from "@chakra-ui/react";
 import { Logo } from "./Logo";
 import { NotificationsNav } from "./NotificationsNav";
 import { Profile } from "./Profile";
 import { SearchBox } from "./SearchBox";
 export function Header() {
+    const isWideVersion = useBreakpointValue({ base: false, lg: true });
+
     return (
         <Flex
             w="100%"
@@ -16,10 +18,10 @@ export function Header() {
             px="6"
         >
             <Logo />
-            <SearchBox />
+            {isWideVersion && <SearchBox />}
             <Flex align="center" ml="auto">
                 <NotificationsNav />
-                <Profile />
+                <Profile showProfileData={isWideVersion} />
             </Flex>
         </Flex>
     );
