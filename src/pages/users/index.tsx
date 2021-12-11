9bcea0655295a08f23a5cbbff5ddc10abe2d89bd
João Henrique <joaohenriquetdj@gmail.com>
2021-12-11 20:10:36 -0300

    Add links

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -12,13 +12,17 @@ import {
     Th,
     Thead,
     Tr,
+    useBreakpointValue,
 } from "@chakra-ui/react";
+import Link from "next/link";
 import { RiAddLine, RiPencilLine } from "react-icons/ri";
 import { Header } from "../../components/Header";
 import { Pagination } from "../../components/Pagination";
 import { Sidebar } from "../../components/Sidebar";
 
 export default function UserList() {
+    const isWideVersion = useBreakpointValue({ base: false, lg: true });
+
     return (
         <Box>
             <Header />
@@ -29,30 +33,32 @@ export default function UserList() {
                         <Heading size="lg" fontWeight="normal">
                             Usuários
                         </Heading>
-                        <Button
-                            as="a"
-                            size="sm"
-                            fontSize="sm"
-                            colorScheme="pink"
-                            leftIcon={<Icon as={RiAddLine} fontSize="20" />}
-                        >
-                            Criar Novo
-                        </Button>
+                        <Link href="/users/create" passHref>
+                            <Button
+                                as="a"
+                                size="sm"
+                                fontSize="sm"
+                                colorScheme="pink"
+                                leftIcon={<Icon as={RiAddLine} fontSize="20" />}
+                            >
+                                Criar Novo
+                            </Button>
+                        </Link>
                     </Flex>
                     <Table colorScheme="whiteAlpha">
                         <Thead>
                             <Tr>
-                                <Th px="6" color="gray.300" w="8">
+                                <Th px={["4", "4", "6"]} color="gray.300" w="8">
                                     <Checkbox colorScheme="pink" />
                                 </Th>
                                 <Th>Usuário</Th>
-                                <Th>Data de cadastro</Th>
+                                {isWideVersion && <Th>Data de cadastro</Th>}
                                 <Th w="8"></Th>
                             </Tr>
                         </Thead>
                         <Tbody>
                             <Tr>
-                                <Td px="6">
+                                <Td px={["4", "4", "6"]}>
                                     <Checkbox colorScheme="pink" />
                                 </Td>
                                 <Td>
@@ -65,22 +71,24 @@ export default function UserList() {
                                         </Text>
                                     </Box>
                                 </Td>
-                                <Td>04 de Abril de 2021</Td>
+                                {isWideVersion && <Td>04 de Abril de 2021</Td>}
                                 <Td>
-                                    <Button
-                                        as="a"
-                                        size="sm"
-                                        fontSize="sm"
-                                        colorScheme="facebook"
-                                        leftIcon={
-                                            <Icon
-                                                as={RiPencilLine}
-                                                fontSize="16"
-                                            />
-                                        }
-                                    >
-                                        Editar
-                                    </Button>
+                                    {isWideVersion && (
+                                        <Button
+                                            as="a"
+                                            size="sm"
+                                            fontSize="sm"
+                                            colorScheme="facebook"
+                                            leftIcon={
+                                                <Icon
+                                                    as={RiPencilLine}
+                                                    fontSize="16"
+                                                />
+                                            }
+                                        >
+                                            Editar
+                                        </Button>
+                                    )}
                                 </Td>
                             </Tr>
                         </Tbody>
