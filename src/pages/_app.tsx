26cd7e36f0340c126598f89d5e2462738ce5c8bd
João Henrique <joaohenriquetdj@gmail.com>
2021-12-12 17:31:05 -0300

    add react query

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,18 +3,23 @@ import { ChakraProvider } from "@chakra-ui/react";
 import { theme } from "../styles/theme";
 import { SideBarDrawerProvider } from "../contexts/SidebarDrawerContext";
 import { makeServer } from "../mirage";
+import { QueryClient, QueryClientProvider } from "react-query";
 
 if (process.env.NODE_ENV === "development") {
     makeServer();
 }
 
+const queryClient = new QueryClient();
+
 function MyApp({ Component, pageProps }: AppProps) {
     return (
-        <ChakraProvider theme={theme}>
-            <SideBarDrawerProvider>
-                <Component {...pageProps} />
-            </SideBarDrawerProvider>
-        </ChakraProvider>
+        <QueryClientProvider client={queryClient}>
+            <ChakraProvider theme={theme}>
+                <SideBarDrawerProvider>
+                    <Component {...pageProps} />
+                </SideBarDrawerProvider>
+            </ChakraProvider>
+        </QueryClientProvider>
     );
 }
 
